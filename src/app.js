3d6c8b1a5b42e7531b69104683bab33fea0a8810
flaviaolivera <126910577+flaviaolivera@users.noreply.github.com>
2023-08-11 10:59:57 +0000

    Función creada, bootstrap actualizado, diseño anterior eliminado, navbar agregado.

--- src/app.js
+++ src/app.js
@@ -5,7 +5,40 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function generarDominio() {
+  //write your code her
+  let partesDelDominio = [
+    ["vuestro", "nuestro", "tu", "mi", "su"],
+    ["dorado", "nuevo", "inocente", "fresco", "resplandeciente"],
+    ["ratón", "teléfono", "comunicación", "mantel", "perro"],
+    [".es", ".com", ".net", ".ar", ".us"]
+  ];
+
+  let miDominio = "";
+  for (let pronombre = 0; pronombre < partesDelDominio[0].length; pronombre++) {
+    for (let adjetivo = 0; adjetivo < partesDelDominio[1].length; adjetivo++) {
+      for (
+        let sustantivo = 0;
+        sustantivo < partesDelDominio[2].length;
+        sustantivo++
+      ) {
+        for (
+          let extension = 0;
+          extension < partesDelDominio[3].length;
+          extension++
+        ) {
+          miDominio +=
+            partesDelDominio[0][pronombre] +
+            partesDelDominio[1][adjetivo] +
+            partesDelDominio[2][sustantivo] +
+            partesDelDominio[3][extension] +
+            " \n";
+        }
+      }
+    }
+  }
+  console.log(miDominio);
+}
+document.addEventListener("DOMContentLoaded", function() {
+  document.querySelector("#Logo").addEventListener("click", generarDominio);
+});
